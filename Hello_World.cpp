9e861c4ac4a74fa51e36801949f8c0d2da5b09fb
hsafwat <heshamsafwat79@gmail.com>
2022-07-23 09:47:20 +0200

    Adding Hello World Code

---
+++ Hello_World.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void add();
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
+	void add()
+	{
+		std::cout << "Adding 2 Numbers" << std::endl;
+	}
